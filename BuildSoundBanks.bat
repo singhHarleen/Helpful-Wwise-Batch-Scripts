6101bc3bd57cea7f2e07fa087d64414eee02b0d9
Harleen Singh <harleen.singh0427@gmail.com>
2024-04-07 19:47:37 +0100

    Update generate soundbanks script to allow user input or use the hard-code path

--- BuildSoundBanks.bat
+++ BuildSoundBanks.bat
@@ -2,8 +2,19 @@
 
 cd /d "%WWISEROOT%\Authoring\x64\Release\bin"
 
+echo Please select an option: 
+echo 1. Use the hard-coded project path
+echo 2. Provid a custom project path
+set /p UserChoice=Type your choice and press Enter: 
+
+if "%UserChoice%"=="1" (
+    set ProjectPath="Hard-Coded-Project-Path"
+) else if "%UserChoice%" == "2" (
+    set /p ProjectPath=Please eneter your custom project path and press Enter:
+)
+
 :: Builds banks for all selected platforms in the project
-wwiseconsole generate-soundbank "Add .wproj directory here" 
+wwiseconsole generate-soundbank %ProjectPath%
 
 echo Soundbank generation completed. 
 
